0099f7b7c0965c4bf95e25b7d7d93c81c23819ad
Antoheredia28 <antonellaherediaia@gmail.com>
2025-09-22 18:05:25 -0300

    eventos-ejemplo

---
+++ events-ejemplo/index.mjs
@@ -0,0 +1,17 @@
+import {EventEmitter} from 'events';
+
+//Crear una instancia de EventEmitter
+
+const emisor = new EventEmitter();
+
+//Definir un evento personalizado
+
+emisor.on ('Saludo', (nombre) =>{
+
+    console.log(`¡Hola ${nombre}!`);
+
+});
+
+//Emitir el evento 'saludo'
+
+emisor.emit('Saludo','Mundo');
